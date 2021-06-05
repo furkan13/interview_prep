f0a2b35a98e2ff26fcd6f70cd5e5fe59f0764561
Furkan Workstation <falahi161212@bscse.uiu.ac.bd>
2021-06-05 18:07:21 +0000

    Sync LeetCode submission - Reverse String (java)

---
+++ my-folder/problems/reverse_string/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void reverseString(char[] s) {
+        char temp;
+        int l=s.length;
+       
+        for(int i=0;i<l/2;i++)
+        {
+            temp=s[i];
+            s[i]=s[l-1-i];
+           
+            s[l-1-i]=temp;
+        }
+        
+    }
+}
