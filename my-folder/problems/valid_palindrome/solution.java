c1a429b483e0fc6959d579de18272f14d6577219
Furkan Workstation <falahi161212@bscse.uiu.ac.bd>
2023-05-24 22:59:49 +0000

    Sync LeetCode submission - Valid Palindrome (java)

---
+++ my-folder/problems/valid_palindrome/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String strUp= s.replaceAll(
+          "[^a-zA-Z0-9]", "");
+
+        String str =strUp.toLowerCase();
+        for(int i=0,j=str.length()-1;i<str.length()/2;i++,j--)
+        {
+            if(str.charAt(i)!=str.charAt(j))
+            {
+                return false;
+            }
+        }
+        return true;
+        
+    }
+}
