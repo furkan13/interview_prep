ab6880bc8c5d32e01ad33c1bf9ed641dbd6f756a
Furkan Workstation <falahi161212@bscse.uiu.ac.bd>
2023-06-01 17:33:04 +0000

    Sync LeetCode submission - Valid Anagram (java)

---
+++ my-folder/problems/valid_anagram/solution.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(t.length()!=s.length())
+        {
+            return false;
+        }
+        int []chars =new int[26];
+        int []sChars=new int [26];
+        for(char c:s.toCharArray())
+        {
+             chars[c-'a']++;
+            //System.out.println(chars[c-'a']);
+        }
+        for(char c:t.toCharArray())
+        {
+            sChars[c-'a']++;
+        }
+        for(int i=0;i<chars.length;i++)
+
+        {
+             
+
+           
+            if(chars[i]!=sChars[i])
+            {
+                return false;
+           }
+        }
+        return true;
+        
+        
+
+
+        
+    }
+}
