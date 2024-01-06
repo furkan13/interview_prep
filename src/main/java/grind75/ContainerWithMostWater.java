e31d2c4931f03ae3523cc498d9196c3360af1608
Furkan <f.alahi@fondy.eu>
2024-01-06 05:45:15 +0000

    Container With Most Water

---
+++ src/main/java/grind75/ContainerWithMostWater.java
@@ -0,0 +1,33 @@
+package grind75;
+//https://leetcode.com/problems/container-with-most-water/submissions/
+public class ContainerWithMostWater {
+    class Solution {
+        public int maxArea(int[] height) {
+            int com=Integer.MIN_VALUE;
+            int res=0;
+            for(int i=0,j=(height.length-1);i<height.length;)
+            {
+                int x=height.length-(height.length-j)-i;
+                System.out.println(j);
+                int y1=height[i];
+                int y2=height[j];
+                if(y1>y2)
+                {
+                    int arr=x*y2;
+                    com=Math.max(arr,com);
+                    j--;
+
+                }
+                else
+                {
+                    int arr=x*y1;
+                    com=Math.max(arr,com);
+                    i++;
+                }
+
+            }
+            return com;
+
+        }
+    }
+}
